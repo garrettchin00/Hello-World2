b4629636b98da59ef1169b104813166b92817aab
Garrett Chin <garrettchin00@gmail.com>
2025-12-17 14:42:59 -0800

    Removed ConsoleApp1

--- ConsoleApp1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-// this is another comment line
-// this line will print Hello, Garrett! to the console
-
-Console.WriteLine("Hello, Garrett!");
-// this line will print Hello World! to the console
-Console.WriteLine("Hello, World!");
