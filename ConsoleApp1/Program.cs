9e56675f4e5d074a69d07380416f7aaf677c647a
Garrett Chin <garrettchin00@gmail.com>
2025-12-17 14:28:03 -0800

    I added Hello World!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // this is another comment line
 // this line will print Hello, Garrett! to the console
+
+using System.Diagnostics;
+
 Console.WriteLine("Hello, Garrett!");
+Console.WriteLine("Hello World!");
