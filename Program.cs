7055e8e42c3fd2e014207be4c8a1c6d97e14a4a1
Garrett Chin <garrettchin00@gmail.com>
2025-12-17 14:48:23 -0800

    Removed duplicated comment and added space

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 // See https://aka.ms/new-console-template for more information
-// See https://aka.ms/new-console-template for more information
 // this is another comment line
 // this line will print Hello, Garrett! to the console
-
 Console.WriteLine("Hello, Garrett!");
 // this line will print Hello World! to the console
 Console.WriteLine("Hello, World!");
